8cf90f4d0a399469acff657d9ebc2fdd648f3610
lluu0029 <lluu0029@student.monash.edu>
2024-03-07 18:25:11 +1100

    Updated name of teams to full name

--- website/create_team_players_dict.py
+++ website/create_team_players_dict.py
@@ -8,13 +8,13 @@ from nba_api.stats.static import teams
 # nba_teams = teams.get_teams()
 # team_dict = {}
 # for team in nba_teams:
-#     team_dict[team['abbreviation']] = team['id']
+#     team_dict[team['full_name']] = team['id']
 
 
 # players_team_dict = {}
-# for item, value in team_dict.items():
+# for key, value in team_dict.items():
 #     time.sleep(1)
-#     players_team_dict[item] = get_players_in_team(value)
+#     players_team_dict[key] = get_players_in_team(value)
 
 # # Saves dictionary of players in a team into team_players_dict.pkl
 # with open('website\\team_players_dict.pkl', 'wb') as file:
